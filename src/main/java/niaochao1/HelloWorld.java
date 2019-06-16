b25f121cc6838b63eec2c426e227fb08555513a2
FengNiao2014 <chen_dunyuan@yahoo.com>
2019-06-15 19:29:07 -0700

    eclipse test commit

---
+++ src/main/java/niaochao1/HelloWorld.java
@@ -0,0 +1,14 @@
+package niaochao1;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(StringUtils.isBlank("    "));
+		
+		System.out.print("Hello World");
+	}
+
+}
